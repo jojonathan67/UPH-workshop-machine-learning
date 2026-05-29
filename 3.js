6ded988c6eb7f8fbc85c99dbd57e97641ce6a473
jojonathan67 <jojo01c12@gmail.com>
2026-05-29 19:36:37 +0700

    Add files via upload

--- 3.js
+++ 3.js
@@ -3,7 +3,7 @@
 const TM_MODEL_URL = "https://teachablemachine.withgoogle.com/models/7VS9HvY20/";
 // API key Gemini dari aistudio.google.com
 // INGAT: key ini terlihat publik di GitHub
-const GEMINI_KEY = "AIzaSyDQmVx0FxZUXthVsQZgPR6okJSPCy-P5hE"; // ganti dengan key kamu
+const GEMINI_KEY = "AQ.Ab8RN6LanDwbEFaY0Je5LSoHqvCv1WtYPfoMvkjakwSZtbouFw"; // ganti dengan key kamu
 // Nama model Gemini yang digunakan
 const GEMINI_MODEL = "gemini-2.5-flash";
 // STATE — variabel yang dibutuhkan oleh beberapa fungsi
